58a9800e066d121f87daa652c23d35cb4ca7db2f
matthewfoucher <matthewfoucher@gmail.com>
2016-10-03 13:51:17 -0700

    Interact with NPC

---
+++ Assets/Scripts/NPCScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NPCScript : MonoBehaviour
+{
+    public GameObject player;
+    public Text dialog;
+    public RawImage box;
+    private bool pressed = false;
+	// Use this for initialization
+	void Start ()
+	{
+	    dialog.text = "";
+        box.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.E))
+	    {
+	        float distance = Vector3.Distance(transform.position, player.transform.position);
+	        if (distance <= 5.0f)
+	        {
+	            pressed = true;
+	            transform.LookAt(player.transform);
+                box.gameObject.SetActive(true);
+	            dialog.text = "We've all been turned into cubes by the three-headed dragon!";
+	        }
+            else if (pressed == true)
+            {
+                pressed = false;
+                box.gameObject.SetActive(false);
+                dialog.text = "";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    // private Rigidbody rb;
+
+    void Start()
+    {
+       // rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.position += (movement * speed);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
