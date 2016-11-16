cc8127828e068fb8bd7604b8393ce0be14feaf05
vogel005 <vogel005@cougars.csusm.edu>
2016-11-15 16:51:05 -0800

    Added sevens current inventory and Dialogue document

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ItemType {MANA, HEALTH};
+
+public class Item : MonoBehaviour 
+{
+    /// <summary>
+    /// The current item type
+    /// </summary>
+    public ItemType type;
+
+    /// <summary>
+    /// The item's neutral sprite
+    /// </summary>
+    public Sprite spriteNeutral;
+
+    /// <summary>
+    /// The item's highlighted sprite
+    /// </summary>
+    public Sprite spriteHighlighted;
+
+    /// <summary>
+    /// The max amount of times the item can stack
+    /// </summary>
+    public int maxSize;
+
+    /// <summary>
+    /// Uses the item
+    /// </summary>
+    public void Use()
+    {
+        switch (type) //Checks which kind of item this is
+        {
+            case ItemType.MANA:
+                Debug.Log("I just used a mana potion");
+                break;
+            case ItemType.HEALTH:
+                Debug.Log("I just used a health potion");
+                break;
+        }
+
+    }
+
+}
