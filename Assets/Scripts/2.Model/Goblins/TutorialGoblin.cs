de774b26a31204bade6fe39b2563fc9673596799
John Vogel <vogel005@cougars.csusm.edu>
2016-11-17 20:54:54 -0800

    Redesigned Dialogue / Lowered Mouse Sens.
    Only the tutorial goblin script should be changed. I made the player get
    freezed in place while they enter dialogue. It is now buttons for
    dialogue choices. Dialogue box is temp.
    
    I also lowered mouse sensitivity.

--- Assets/Scripts/2.Model/Goblins/TutorialGoblin.cs
+++ Assets/Scripts/2.Model/Goblins/TutorialGoblin.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 public class TutorialGoblin : MonoBehaviour
 {
     public GameObject player;
-    public Text dialog1;
-    public Text dialog2;
-    public Text dialog3;
+    public Text dialog0;
+
+    public Button dialog1;
+    public Button dialog2;
+
     public RawImage box;
     private bool pressed = false;
     private Seek aggro;
@@ -15,11 +17,7 @@ public class TutorialGoblin : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        dialog1.text = "";
-        dialog2.text = "";
-        dialog3.text = "";
         box.gameObject.SetActive(false);
-        dialogueLevel = 0;
         aggro = GetComponent<Seek>();
     }
 
@@ -28,92 +26,86 @@ public class TutorialGoblin : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            GameObject.Find("Player").GetComponent<PlayerAttack>().enabled = false;
+            GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
+            GameObject.Find("Main Camera").GetComponent<SmoothMouseLook>().enabled = false;
+
             float distance = Vector3.Distance(transform.position, player.transform.position);
             if (distance <= 5.0f)
             {
                 pressed = true;
                 transform.LookAt(player.transform);
                 box.gameObject.SetActive(true);
-                dialog1.text = "Yaargh, welcome, welcome. If yargh confused, let me break down thar controls.";
-                dialog2.text = "1. Um, I would rather not.";
-                dialog3.text = "2. ...What?";
-                dialogueLevel = 1;
+                dialog0.gameObject.SetActive(true);
+                dialog1.gameObject.SetActive(true);
+                dialog2.gameObject.SetActive(true);
 
+                dialog0.text = "Yaargh, welcome, welcome. If yargh confused let me break down thar controls.";
+                dialog1.GetComponentInChildren<Text>().text = "Um, I would rather not.";
+                dialog2.GetComponentInChildren<Text>().text = "... What?";
+
+                dialog1.onClick.AddListener(Um);
+                dialog2.onClick.AddListener(What);
             }
             else if (pressed == true)
             {
                 pressed = false;
                 box.gameObject.SetActive(false);
-                dialog1.text = "";
-                dialog2.text = "";
-                dialog3.text = "";
             }
         }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            switch (dialogueLevel)
-            {
-                case 1:
-                    dialog1.text = "";
-                    dialog2.text = "";
-                    dialog3.text = "";
-                    dialogueLevel = 0;
-                    box.gameObject.SetActive(false);
-                    break;
-                case 2:
-                    dialog1.text = "Move yaargh mousie around to move thar cam. But that isn't available in this version yet! Left click to attack to yaargh heart's desire. Move thar cross hairs over an item to put it in yur pocket (coming soon), or over someone and press E to talkie... but you musta figured that out already to talk to me yarharhar.";
-                    dialog2.text = "1. Mousies... WHAT? I don't understand what you're saying. Why are you green, too?";
-                    dialog3.text = "";
-                    dialogueLevel = 3;
-                    break;
-                case 3:
-                    dialog1.text = "Yarhar... Press Q to use tha health potions. And mostie importantly, press I to open up thar inventory to see all yees items, and J for thar questies. The developers have been sleeping a lot lately, so some of these features might not work yet.\n\nOh, I'ma green cus I'ma a goblin. FITE ME!!";
-                    dialog2.text = "";
-                    dialog3.text = "";
-                    dialogueLevel = 4;
-                    break;
-                case 4:
-                    dialog1.text = "";
-                    dialog2.text = "";
-                    dialog3.text = "";
-                    box.gameObject.SetActive(false);
-                    aggro.hostile = true;
-                    break;
-                default:
-                    break;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            switch (dialogueLevel)
-            {
-                case 1:
-                    dialog1.text = "WASD to move, of course. Space to hop hop. Shift to slide around real quickie.";
-                    dialog2.text = "1. What are you talking about?";
-                    dialog3.text = "";
-                    dialogueLevel = 2;
-                    break;
-                case 2:
-                    dialog1.text = "";
-                    dialog2.text = "";
-                    dialog3.text = "";
-                    break;
-                case 3:
-                    dialog1.text = "";
-                    dialog2.text = "";
-                    dialog3.text = "";
-                    dialogueLevel = 4;
-                    break;
-                case 4:
-                    dialog1.text = "";
-                    dialog2.text = "";
-                    dialog3.text = "";
-                    box.gameObject.SetActive(false);
-                    aggro.hostile = true;
-                    break;
-                default:
-                    break;
-            }
-        }
+    }
+
+    void Um()
+    {
+        dialog0.gameObject.SetActive(false);
+        dialog1.gameObject.SetActive(false);
+        dialog2.gameObject.SetActive(false);
+        box.gameObject.SetActive(false);
+
+        GameObject.Find("Player").GetComponent<PlayerAttack>().enabled = true;
+        GameObject.Find("Player").GetComponent<PlayerController>().enabled = true;
+        GameObject.Find("Main Camera").GetComponent<SmoothMouseLook>().enabled = true;
+    }
+
+    void What()
+    {
+        dialog0.text = "WASD to move of course. Space to hop hop. Shift to slide around real quickie.";
+        dialog1.GetComponentInChildren<Text>().text = "What are you talking about?";
+        dialog2.gameObject.SetActive(false);
+
+        dialog1.onClick.RemoveAllListeners();
+        dialog1.onClick.AddListener(Whatare);
+    }
+
+    void Whatare()
+    {
+        dialog0.text = "Move yaargh mousie around to move thar cam. Left click to attack to hearts desire. Move thar cross hairs over an item to put it in yur pocket and over someone and press E to talkie... but you musta figured that out already to talk to me yarharhar.";
+        dialog1.GetComponentInChildren<Text>().text = "Mousies... WHAT? I don't understand what you're saying. Why are you green too?";
+        dialog2.gameObject.SetActive(false);
+
+        dialog1.onClick.RemoveAllListeners();
+        dialog1.onClick.AddListener(Mousies);
+    }
+
+    void Mousies()
+    {
+        dialog0.text = "Yarhar... Press Q to use tha health potions. And mostie importantly press I to open up thar inventory to see all yees items and J for thar questies. \n\n Oh Ima green cus Ima a goblin. FITE ME!!";
+        dialog1.GetComponentInChildren<Text>().text = "BRING IT!";
+        dialog2.gameObject.SetActive(false);
+        dialog1.onClick.RemoveAllListeners();
+
+        dialog1.onClick.RemoveAllListeners();
+        dialog1.onClick.AddListener(Bring);
+    }
+
+    void Bring()
+    {
+        box.gameObject.SetActive(false);
+        dialog0.gameObject.SetActive(false);
+        aggro.hostile = true;
+
+        GameObject.Find("Player").GetComponent<PlayerAttack>().enabled = true;
+        GameObject.Find("Player").GetComponent<PlayerController>().enabled = true;
+        GameObject.Find("Main Camera").GetComponent<SmoothMouseLook>().enabled = true;
     }
 }
