95ced8804396d4bf5fd4e38947f1e0092ce603df
vogel005 <vogel005@cougars.csusm.edu>
2016-12-13 17:20:49 -0800

    Added Dovahkiid Companion
    Dovahkiid now joins in jolly cooperation!
    
    Created dovahkiidattack for him to attack the dragons in a row.
    He will be activated by his quest + rock trigger.
    
    Also forgot to mention I added a differing cooldown for each of the
    dragons so they aren't always attacking at once.
    
    Changed back the dragon's box collider to non-trigger, as the
    dialoguebox collider fixes the whole problem.
    
    Increased fireball explosion radius from 10-15 because you could hide in
    front of Mr Pink.

--- Curse of Cubes Unity Project/Assets/Scripts/4.Controllers/BossTrigger.cs
+++ Curse of Cubes Unity Project/Assets/Scripts/4.Controllers/BossTrigger.cs
@@ -5,22 +5,17 @@ public class BossTrigger : MonoBehaviour
 {
 
     public GameObject rock; // Reference to the rock object.
-
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public GameObject kiid;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             rock.SetActive(true); // If the player walks through the trigger, spawn a big rock so the player cannot leave the dragon's cave.
-        }
+			if (Quests.dovahkiid == 3) //If the dovahkiid quest was accepted, activate him.
+			{
+				kiid.SetActive (true);
+			}
+		}
     }
 }
---
+++ Curse of Cubes Unity Project/Assets/Scripts/4.Controllers/DovahkiidAttack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DovahkiidAttack : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float rotSpeed = 2.0f;
+
+	//Dragons to target
+	public Transform target1;
+	public Transform target2;
+	public Transform target3;
+
+	// Update is called once per frame
+	void Update () {
+		if (Quests.dragonCount == 3) 
+		{
+			//Rotates to look at the dragon
+			transform.LookAt(target3.transform);
+
+			//Go Forward
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		}
+		if (Quests.dragonCount == 2) 
+		{
+			//Rotates to look at the dragon
+			transform.LookAt(target2.transform);
+
+			//Go Forward
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		}
+		if (Quests.dragonCount == 1) 
+		{
+			//Rotates to look at the dragon
+			transform.LookAt(target1.transform);
+
+			//Go Forward
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		}
+	}
+}
